90689316e43d964852679fe2edc774eb938bb54f
Neo777Uzy <uzay94.777@gmail.com>
2025-10-26 16:47:42 +0300

    Update uzy_main.js

--- js/uzy_main.js
+++ js/uzy_main.js
@@ -1,6 +1,4 @@
 const btn_header_signup = document.querySelector('.header__signup');
 
-if (btn_header_signup) {
   btn_header_signup.classList.remove('header__signup');
   btn_header_signup.classList.add('uzy_button', 'uzy_dark_2');
-}
